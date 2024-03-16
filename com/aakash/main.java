e7a76922731f3d1909625811cac3fce1523ae1d3
aakashloyar <aakashloyar@gmail.com>
2024-03-16 13:38:40 +0530

    added build, min and update query

--- com/aakash/main.java
+++ com/aakash/main.java
@@ -2,6 +2,60 @@ package com.aakash;
 
 public class main {
     public static void main(String[] args) {
-       
+        int n=6;
+        int[] arr=new int[]{2,1,0,5,4,3};
+        Segment seg1=new Segment(n,arr);
+        seg1.build(arr,0,0,n-1);
+        int min=seg1.min(0,n-1,0,0,n-1);
+        System.out.println(min);
+        seg1.update(0,0,n-1,2,4);
+        seg1.update(0,0,n-1,0,4);
+        seg1.update(0,0,n-1,1,4);
+        min=seg1.min(0,n-1,0,0,n-1);
+        System.out.println(min);
     }
+}
+class Segment {
+    int[] seg;
+    int n;
+    Segment(int n,int[] seg) {
+        this.n=n;
+        this.seg=new int[4*n];
+    }
+    int build(int[] arr,int ind,int low,int high) {
+        //left ind=2*ind+1
+        //right ind=2*ind+2
+        //mid=(low+high)/2
+        if(high-low==0) {
+            seg[ind]=arr[low];
+            return seg[ind];
+        }
+        int mid=low+(high-low)/2;
+        int leftind=(2*ind)+1;
+        int rightind=(2*ind)+2;
+        seg[ind]=Math.min(build(arr,leftind,low,mid),build(arr,rightind,mid+1,high));
+        return seg[ind];
+    }
+    int min(int s,int e,int ind,int low,int high) {
+        //no overlapping
+        if(high<s ||e<low) return Integer.MAX_VALUE;
+        //complete merge
+        if(s<=low &&high<=e) return seg[ind];
+        int mid=low+(high-low)/2;
+        int leftind=2*ind+1;
+        int rightind=2*ind+2;
+        return Math.min(min(s,e,leftind,low,mid),min(s,e,rightind,mid+1,high));
+    }//return minimum of given range in array
+    void update(int ind,int low,int high,int pivot,int val) {
+        if(low==high) {
+            seg[ind]=val;
+            return;
+        }
+        int mid=low+(high-low)/2;
+        int leftind=2*ind+1;
+        int rightind=2*ind+2;
+        if(pivot<=mid) update(leftind,low,mid,pivot,val);
+        else update(rightind,mid+1,high,pivot,val);
+        seg[ind]=Math.min(seg[rightind],seg[leftind]);
+    }//update the array on given index
 }
